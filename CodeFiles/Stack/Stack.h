11a5e4f5856c268d739e8346f4392666ba14a76b
Nour <nouraly28898@gmail.com>
2020-12-27 15:31:37 +0200

    stackTest

--- CodeFiles/Stack/Stack.h
+++ CodeFiles/Stack/Stack.h
@@ -1,50 +1,56 @@
 // C++ program for linked list implementation of stack
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 // A structure to represent a stack
-class StackNode {
+class StackNode
+{
 public:
     int data;
-    StackNode* next;
+    StackNode *next;
 };
- 
-StackNode* newNode(int data)
+
+StackNode *newNode(int data)
 {
-    StackNode* stackNode = new StackNode();
+    StackNode *stackNode = new StackNode();
     stackNode->data = data;
     stackNode->next = NULL;
     return stackNode;
 }
- 
-int isEmpty(StackNode* root)
+
+int isEmpty(StackNode *root)
 {
     return !root;
 }
- 
-void push(StackNode** root, int data)
+
+void push(StackNode **root, int data)
 {
-    StackNode* stackNode = newNode(data);
+    StackNode *stackNode = newNode(data);
     stackNode->next = *root;
     *root = stackNode;
-    cout << data << " pushed to stack\n";
 }
- 
-int pop(StackNode** root)
+
+int pop(StackNode **root)
 {
     if (isEmpty(*root))
+    {
+
         return INT_MIN;
-    StackNode* temp = *root;
+    }
+    StackNode *temp = *root;
     *root = (*root)->next;
     int popped = temp->data;
     free(temp);
- 
+
     return popped;
 }
- 
-int peek(StackNode* root)
+
+int peek(StackNode *root)
 {
     if (isEmpty(root))
+    {
+
         return INT_MIN;
+    }
     return root->data;
 }
---
+++ TestScripts/Stack_Test.cpp
@@ -0,0 +1,99 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../CodeFiles/Stack/Stack.h"
+#include "../Tool/doctest.h"
+#include <iostream>
+using namespace std;
+
+TEST_SUITE("Correctness")
+{
+    TEST_CASE("Push item in stack and pop it off")
+    {
+        StackNode *stack = NULL;
+
+        push(&stack, 4);
+        REQUIRE_EQ(pop(&stack), 4);
+    }
+
+    TEST_CASE("Push multiple items and pop them and check if reversed order")
+    {
+        StackNode *stack = NULL;
+
+        for (int i = 0; i < 7; i++)
+        {
+            push(&stack, i);
+        }
+
+        for (int i = 6; i >= 0; i--)
+        {
+            REQUIRE_EQ(pop(&stack), i);
+        }
+    }
+    TEST_CASE("Uninitialized stack is empty")
+    {
+
+        StackNode *stack = NULL;
+        REQUIRE_EQ(isEmpty(stack), true);
+    }
+
+    TEST_CASE("Stack with pushed item is not empty")
+    {
+        StackNode *stack = NULL;
+
+        push(&stack, 4);
+        REQUIRE_EQ(isEmpty(stack), false);
+    }
+
+    TEST_CASE("Stack with pushed items and then poped is empty")
+    {
+        StackNode *stack = NULL;
+
+        for (int i = 0; i < 7; i++)
+        {
+            push(&stack, i);
+        }
+
+        for (int i = 6; i >= 0; i--)
+        {
+            pop(&stack);
+        }
+        REQUIRE_EQ(isEmpty(stack), true);
+    }
+    TEST_CASE("Pop from empty stack")
+    {
+        StackNode *stack = NULL;
+
+        CHECK_NOTHROW(pop(&stack));
+    }
+
+    TEST_CASE("The peek of stack is the last item pushed")
+    {
+
+        StackNode *stack = NULL;
+
+        for (int i = 0; i < 7; i++)
+        {
+            push(&stack, i);
+        }
+        REQUIRE_EQ(peek(stack), 6);
+    }
+    TEST_CASE("The peek of empty stack")
+    {
+        StackNode *stack = NULL;
+
+        CHECK_NOTHROW(peek(stack));
+        REQUIRE_EQ(peek(stack), INT_MIN);
+    }
+}
+
+TEST_SUITE("Performance")
+{
+    TEST_CASE("Time Complexity: O(n) where n is the number of elements to push in the stack." * doctest::timeout(0.5))
+    {
+
+        StackNode *stack = NULL;
+        for (int i = 0; i < 1e7; i++)
+        {
+            push(&stack, i);
+        }
+    }
+}
